4ac55c35cf542aba7ba6cd186a7f26aa0a94f3dd
Alex <alex@surfstudio.com>
2019-06-29 15:00:32 +1000

    Updated Create bug endpoint to cleaner format
    -Integrated the label provided to be displayed on Trello.
    
    -Updated the MarkDown formatting of the bugs description

--- bug/bug.js
+++ bug/bug.js
@@ -1,25 +1,40 @@
 
 const request = require('request');
 const { TOKEN, KEY } = require('../credentials');
-const { BUG_LIST_ID } = require('../global');
+const { BUG_LIST_ID, TRIVIAL_LABEL, MINOR_LABEL, MAJOR_LABEL, CRITICAL_LABEL } = require('../global');
 
 
 //Creates the bug as a card in Trello
 async function createBug(bug){
+  //decide which label should be chosen
+  switch( bug.label ){
+    case 'critical':
+      bug.labelID = CRITICAL_LABEL;
+      break;
+    case 'major':
+      bug.labelID = MAJOR_LABEL;
+      break;
+    case 'minor':
+      bug.labelID = MINOR_LABEL;
+      break;
+    case 'trivial':
+      bug.labelID = TRIVIAL_LABEL;
+      break; 
+    default:
+      throw new Error('Given label is invalid.');
+  }
+  
   const options = { method: 'POST',
     url: 'https://api.trello.com/1/cards',
-    qs: 
-    { name: bug.name,
-      desc: `
-      summary: ${bug.summary},
-      steps to produce: ${bug.steps_to_produce},
-      expected result: ${bug.expected_result},
-      actual result: ${bug.actual_result},
-      notes: ${bug.actual_result}
-      `,
+    qs: { 
+      name: `[${bug.context}] ${bug.name}`,
+      desc: `###Summary:\n${bug.summary}\n\n###Steps To Produce:\n${bug.steps_to_produce}\n\n###Expected Result:\n${bug.expected_result}\n\n###Actual Result:\n${bug.actual_result}\n\n###Notes:\n${bug.notes}`,
+      idLabels: bug.labelID,
       idList: BUG_LIST_ID,
       key: KEY,
-      token: TOKEN } };
+      token: TOKEN 
+    }
+  };
 
   return new Promise((resolve, reject) => {
     request(options, function (error, response, body) {
